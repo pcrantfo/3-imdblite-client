6cc4e6906c52009f55f727f40dfb2f80a6a0b3c2
pcrantfo <pcrantfo@gmail.com>
2021-05-28 11:24:20 -0600

    Create main-view, movie-view and movie-card files
    Display list of movies and allow user to click one to see more information about it, as well as click Back to go back to the movie view

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class MovieCard extends React.Component {
+    render() {
+        const { movieData, onMovieClick } = this.props;
+        return <div className="movie-card" 
+            onClick={() => 
+                { onMovieClick(movieData);}
+            }>{movieData.title}
+            </div>;
+    }
+}
