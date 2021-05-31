903c8f46868e9282bf9a50d722cf33e95e5d5a12
pcrantfo <pcrantfo@gmail.com>
2021-05-31 12:56:35 -0600

    Update main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -9,20 +11,9 @@ export default class MainView extends React.Component {
         super();
         this.state = {
             movies: [],
-            selectedMovie: null
+            selectedMovie: null,
+            user: null
         };
-    }
-
-    componentDidMount() {
-        axios.get('https://imdblite.herokuapp.com/movies')
-            .then(response => {
-                this.setState({
-                    movies: response.data
-                });
-            })
-            .catch(error => {
-                console.error(error);
-            });
     }
 
     setSelectedMovie(newSelectedMovie) {
@@ -30,9 +21,17 @@ export default class MainView extends React.Component {
             selectedMovie: newSelectedMovie
         });
     }
+
+    onLoggedIn(user) {
+        this.setState({
+            user
+        });
+    }
     
     render() {
-        const { movies, selectedMovie } = this.state;
+        const { movies, selectedMovie, user } = this.state;
+
+        if (!user) return <LoginView onLoggedIn = {user => this.onLoggedIn(user)} />
 
         if (movies.length === 0) return <div className="main-view" />;
 
@@ -53,5 +52,17 @@ export default class MainView extends React.Component {
                 }
             </div>
         );
+    }
+
+    componentDidMount() {
+        axios.get('https://imdblite.herokuapp.com/movies')
+            .then(response => {
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(error => {
+                console.error(error);
+            });
     }
 }
