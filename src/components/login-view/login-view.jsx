117d5d2f7dfc7595d853b4355a74836f9cfc4b15
pcrantfo <pcrantfo@gmail.com>
2021-06-07 10:59:54 -0600

    Style login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
@@ -14,30 +16,37 @@ export function LoginView(props) {
     }
 
     return (
-        <Form>
-            <Form.Group controlId="formUsername">
-                <Form.Label>Username:</Form.Label>
-                <Form.Control 
-                    type="Text"
-                    onChange = { e => 
-                        setUsername(e.target.value)}
-                />
-            </Form.Group>
-            <Form.Group controlId="formPassword">
-                <Form.Label>Password:</Form.Label>
-                <Form.Control 
-                    type="password"
-                    onChange = { e => 
-                        setPassword(e.target.value)}
-                />
-            </Form.Group>
-            <Button 
-                variant="primary"
-                type="submit"
-                onClick={ handleSubmit }
-            >
-                Submit
-            </Button>
-        </Form>
+        <Row className="login-view justify-content-md-center">
+            <Col md={8}>
+            <Row className="justify-content-center">
+                <h1>Login</h1>
+            </Row>
+                <Form>
+                    <Form.Group controlId="formUsername">
+                        <Form.Label>Username:</Form.Label>
+                        <Form.Control 
+                            type="Text"
+                            onChange = { e => 
+                                setUsername(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formPassword">
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control 
+                            type="password"
+                            onChange = { e => 
+                                setPassword(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Button 
+                        variant="primary"
+                        type="submit"
+                        onClick={ handleSubmit }
+                    >
+                        Submit
+                    </Button>
+                </Form>
+            </Col>
+        </Row>
     );
 }
