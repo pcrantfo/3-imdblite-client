e27ba34c148e346293d8e5a373cb9053ac7b52b0
pcrantfo <pcrantfo@gmail.com>
2021-06-07 09:31:21 -0600

    Put movie-card in a card component

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+
 import PropTypes from 'prop-types';
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
     render() {
         const { movieData, onMovieClick } = this.props;
-        return <div className="movie-card" 
-            onClick={() => 
-                { onMovieClick(movieData);}
-            }>{movieData.title}
-            </div>;
+
+        return (
+            <Card>
+                <Card.Img 
+                    variant="top" 
+                    src={ movieData.imgURL } 
+                />
+                <Card.Body>
+                    <Card.Title>{ movieData.title }</Card.Title>
+                    <Card.Text>{ movieData.description }</Card.Text>
+                    <Button 
+                        onClick = {() => 
+                            onMovieClick(movieData)}
+                        variant="link"
+                    >Open
+                    </Button>
+                </Card.Body>
+            </Card>
+        );
     }
 }
 
